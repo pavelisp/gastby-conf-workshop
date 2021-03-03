d57edc7afd099b79e3d9a5ccd2b6268eff855a18
pavelisp <isp.pavel@gmail.com>
2021-03-03 13:12:32 -0500

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+const About = () => {
+  return (
+  <h1>I'm learning VIM as I do this, so this is going really slow! EXTREME! </h1>
+  )
+
+}
+
+export default About 
