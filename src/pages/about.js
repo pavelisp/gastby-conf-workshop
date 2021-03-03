c62def74359df4cb4df042b0923c9d3f662c09d2
pavelisp <isp.pavel@gmail.com>
2021-03-03 13:25:33 -0500

    Added links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
-const About = () => {
+const about = () => {
   return (
   <h1>I'm learning VIM as I do this, so this is going really slow! EXTREME! </h1>
+  <Link to='/'> Go back home </Link>
   )
 
 }
 
-export default About 
+export default about 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 const IndexPage = () => {
   return (
   <h1>Congratulations, you've made it here finally...</h1>
+	  <Link to='/about'> Pavel's Abouts </Link>
   )
 
 }
